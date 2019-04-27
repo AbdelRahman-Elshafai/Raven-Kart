7b0815be98b2506656bd7127adad19344b706a90
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-04-27 22:04:13 +0200

    add template and shared views

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,15 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( plugins.css.scss)
+Rails.application.config.assets.precompile += %w( style.css)
+Rails.application.config.assets.precompile += %w( custom.css)
+Rails.application.config.assets.precompile += %w( js/vendor/modernizr-3.5.0.min.js )
+Rails.application.config.assets.precompile += %w( js/vendor/jquery-3.2.1.min.js )
+Rails.application.config.assets.precompile += %w( js/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( js/popper.min.js )
+Rails.application.config.assets.precompile += %w( js/plugins.js )
+Rails.application.config.assets.precompile += %w( js/active.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
