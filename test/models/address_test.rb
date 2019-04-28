f10600a18f4caf06e1099dea3b085fdffa64d087
mahydit <mahydit_anber@outlook.com>
2019-04-28 02:22:15 +0200

    add test method for creation in users and addresses

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save address" do
+    address = Address.new(
+      building: addresses(:one).building,
+      street: addresses(:one).street,
+      district: addresses(:one).district,
+      city: addresses(:one).city,
+      user_id: 1
+    )
+    assert address.save
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create user" do
+    user = User.new(
+      name: users(:one).name,
+      email: users(:one).email,
+      avatar: users(:one).avatar,
+      role_id: 1
+    )
+    assert user.save
+  end
 end
