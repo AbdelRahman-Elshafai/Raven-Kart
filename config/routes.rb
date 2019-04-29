e7445d85c3d8a91a0cbe71dfb1876198dc3c0aed
mahydit <mahydit_anber@outlook.com>
2019-04-29 19:47:01 +0200

    bug fix: add delete devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, {class_name: 'User'}.merge(ActiveAdmin::Devise.config)
   ActiveAdmin.routes(self)
+  devise_for :users 
   resources :products
   get 'template/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
