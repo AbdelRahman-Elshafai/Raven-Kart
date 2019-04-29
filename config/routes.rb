9ae80511082c90f1d4d8dd1c9b3b20a81466d457
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-04-30 00:11:32 +0200

    fix admin logout bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # devise_for :admin_users, {class_name: 'User'}.merge(ActiveAdmin::Devise.config)
   devise_for :users
   ActiveAdmin.routes(self)
+  devise_scope :user do
+    get '/users/sign_out' , :to => 'active_admin/devise/sessions#destroy'
+  end
   get 'template/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'template/single'
