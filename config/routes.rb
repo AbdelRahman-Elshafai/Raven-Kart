583f1cce734635c9ce7d1b41a1eb14138a23b5a4
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-04-29 22:55:31 +0200

    remove duplicate admin with devise route and add confirmation mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, {class_name: 'User'}.merge(ActiveAdmin::Devise.config)
-  ActiveAdmin.routes(self)
+  # devise_for :admin_users, {class_name: 'User'}.merge(ActiveAdmin::Devise.config)
   devise_for :users
+  ActiveAdmin.routes(self)
   get 'template/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'template/single'
