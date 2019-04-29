5cfc39b32276b6119c5c5abdf4d03c403333963c
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-04-29 11:38:37 +0200

    create user using admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation , :role_id
 
   index do
     selectable_column
     id_column
     column :email
+    column :role_id
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -17,10 +18,12 @@ ActiveAdmin.register User do
   filter :created_at
 
   form do |f|
+    f.semantic_errors *f.object.errors.keys
     f.inputs do
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :role_id, :as => :select, :collection => Role.pluck(:kind , :id)
     end
     f.actions
   end
