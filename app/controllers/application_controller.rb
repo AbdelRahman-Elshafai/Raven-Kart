409a302bc2a32dd8e62e934213f4c55f37b99c81
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-04-29 19:28:41 +0200

    redirect admin to dashboard with devise login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if current_user.role_id == 1
+      admin_root_path
+    else
+      root_path
+    end
+  end
 end
