fb7fbf87c0aa4d3bce3352b51750d7310ca55d8e
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-04-29 20:53:59 +0200

    config action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,13 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # action mailer configs with devise  To enable sending
+  config.action_mailer.perform_deliveries = true
+
+  # action mailer delivery method
+  config.action_mailer.delivery_method = :smtp
+
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
