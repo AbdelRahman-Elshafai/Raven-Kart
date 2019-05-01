536780e1809d926573349a4f1ee02797a6e570ce
mahydit <mahydit_anber@outlook.com>
2019-05-01 23:42:37 +0200

    add cart product resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' , :to => 'active_admin/devise/sessions#destroy'
   end
   resources :products
+  resources :cart_products
   get 'template/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'template/single'
