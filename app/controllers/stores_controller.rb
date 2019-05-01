f845302b546034d9891b3bbfe088c018c52dd71b
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-05-01 12:07:38 +0200

    Add store controller

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,14 @@
+class StoresController < ApplicationController
+  # GET /stores
+  # GET /stores.json
+  def index
+    @stores = Store.all
+  end
+
+  # GET /stores/1
+  # GET /stores/1.json
+  def show
+    @store = Store.find(params[:id])
+    @products = @store.products
+  end
+end
