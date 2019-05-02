c88cb49e8eb4ce9a904e46d8c529151a72475406
mahydit <mahydit_anber@outlook.com>
2019-05-02 15:10:31 +0200

    add add_to_cart method for products

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,5 +1,5 @@
 class CartProductsController < ApplicationController
-  before_action :set_cart_product, only: [:show, :edit, :update, :destroy]
+  # before_action :set_cart_product, only: [:show, :edit, :update, :destroy]
 
   # GET /cart_products
   # GET /cart_products.json
@@ -14,40 +14,30 @@ class CartProductsController < ApplicationController
 
   # GET /cart_products/new
   def new
-    @cart_product = current_user.shopping_cart.cart_products.new
+    # @cart_product = current_user.shopping_cart.cart_products.new
   end
 
   # GET /cart_products/1/edit
   def edit
   end
 
-  # POST /cart_products
-  # POST /cart_products.json
   def create
-    redirect_to products_path
+    # raise cart_product_params['product_id'].inspect
     @cart_product = current_user.shopping_cart.cart_products.new(cart_product_params)
-    respond_to do |format|
-      if @cart_product.save
-        format.html { redirect_to @cart_product, notice: 'Cart product was successfully created.' }
-        format.json { render :show, status: :created, location: @cart_product }
-      else
-        format.html { render :new }
-        format.json { render json: @cart_product.errors, status: :unprocessable_entity }
-      end
+    if @cart_product.save
+      # TODO: redirect to shopping cart page
+      redirect_to products_path, notice: 'Product was added to your cart successfully.' 
+    else
+      redirect_to product_path(params[:cart_product][:product_id]), alert: 'Product wasn\'t  added to your cart, please use valid inputs.'
     end
   end
 
-  # PATCH/PUT /cart_products/1
-  # PATCH/PUT /cart_products/1.json
   def update
-    respond_to do |format|
-      if @cart_product.update(cart_product_params)
-        format.html { redirect_to @cart_product, notice: 'Cart product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cart_product }
-      else
-        format.html { render :edit }
-        format.json { render json: @cart_product.errors, status: :unprocessable_entity }
-      end
+    if get_cart_product.update(update_quantity)
+      # TODO: redirect to shopping cart page
+      redirect_to products_path, notice: 'Product was added to your cart successfully.'
+    else
+      redirect_to product_path(params[:cart_product][:product_id]), alert: 'Product wasn\'t  added to your cart, please try again later.'
     end
   end
 
@@ -61,6 +51,18 @@ class CartProductsController < ApplicationController
     end
   end
 
+  def add_to_cart
+    if !products_in_cart.present? or !get_cart_product
+      create
+    else
+      if product_stock < update_quantity[:quantity]
+        redirect_to product_path(params[:cart_product][:product_id]), alert: 'You added more than the amount available in stock.'
+      else
+        update
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart_product
@@ -69,6 +71,29 @@ class CartProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_product_params
-      params.fetch(:cart_product, {})
+      params.require(:cart_product).permit(:quantity, :product_id)
+    end
+    
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def cart_product_update_params
+      params.require(:cart_product).permit(:quantity)
+    end
+
+    def update_quantity
+      params[:cart_product][:quantity] = (get_cart_product.quantity).to_i + params[:cart_product][:quantity].to_i
+      cart_product_update_params
+    end
+
+    def products_in_cart
+      current_user.shopping_cart.cart_products
+    end
+
+    def get_cart_product
+      products_in_cart.find_by_product_id(cart_product_params[:product_id])
+    end
+
+    def product_stock
+      @product = Product.find_by_id(cart_product_params[:product_id])
+      @product.stock
     end
 end
