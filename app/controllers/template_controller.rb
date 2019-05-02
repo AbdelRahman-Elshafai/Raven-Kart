7bb607c1cf6c60bd48c2cc11cbaad65d1092f4f7
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-02 18:35:48 +0200

    searched results using sunspot

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -5,10 +5,19 @@ class TemplateController < ApplicationController
   end
 
   def search_products
+    @search = Product.solr_search do |searcher|
+      searcher.fulltext search_params[:search]
+    end
+
+    puts @search.results
   end
 
   def new_products
     render "template/shop-grid"
 
+  end
+
+  def search_params
+    params.permit(:search)
   end
 end
