6450295c7c34d083fee9218a777318ae5b395d85
mahydit <mahydit_anber@outlook.com>
2019-05-02 15:10:49 +0200

    add validation on cart_product

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,4 +1,8 @@
 class CartProduct < ApplicationRecord
   belongs_to :product
   belongs_to :shopping_cart
+
+  validates :product_id, product_existance: true
+  validates :quantity, 
+    numericality: { only_integer: true, greater_than: 0 }
 end
