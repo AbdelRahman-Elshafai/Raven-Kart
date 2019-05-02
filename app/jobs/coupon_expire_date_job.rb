39780a682e2038f4b7db89aeea00b9075b684374
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-02 17:20:40 +0200

    add couponExpireDateJob function

--- app/jobs/coupon_expire_date_job.rb
+++ app/jobs/coupon_expire_date_job.rb
@@ -1,8 +1,12 @@
 class CouponExpireDateJob
   def self.perform
+    # puts DateTime.now
     # Do something later
-    coupons = Coupon.all
+    coupons = Coupon.where(expired: false )
     coupons.each do |coupon|
+      if coupon.expiration_date < Time.now.to_date
+          coupon.update(expired:true)
+      end
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_135944) do
+ActiveRecord::Schema.define(version: 2019_05_02_150945) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -83,12 +83,13 @@ ActiveRecord::Schema.define(version: 2019_05_02_135944) do
 
   create_table "coupons", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.datetime "expiration_date"
+    t.date "expiration_date"
     t.integer "usage_limit"
     t.string "deduction_type"
     t.integer "deduction_amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "expired"
   end
 
   create_table "feedbacks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
