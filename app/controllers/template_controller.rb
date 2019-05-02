800c269d76b887571ad753cc1f1b7c7676ffb955
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-03 01:38:40 +0200

    add filter with category and brand

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -4,8 +4,20 @@ class TemplateController < ApplicationController
   def single
   end
   def shopping
+
     @categories = Category.all
     @brands = Brand.all
-    @products = Product.all
+    if params[:category]
+      @category_id = Category.find_by(name: params[:category]).id
+      @products = Product.where(category_id: @category_id)
+    elsif params[:brand]
+      @brand_id = Brand.find_by(name: params[:brand]).id
+      @products = Product.where(brand_id:@brand_id)
+    else
+      @products = Product.all
+    end
+  end
+  def product_params
+    params.fetch(:product, {}).permit(:title, :description, :price, :stock, :brand_id, :category_id, images:[])
   end
 end
