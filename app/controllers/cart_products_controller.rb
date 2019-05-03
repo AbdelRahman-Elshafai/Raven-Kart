3335676257e7093930fb4e88e09959b59a3cd286
mahydit <mahydit_anber@outlook.com>
2019-05-03 02:01:27 +0200

    bug fix: check stock before creation

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,62 +1,15 @@
 class CartProductsController < ApplicationController
-  # before_action :set_cart_product, only: [:show, :edit, :update, :destroy]
-
-  # GET /cart_products
-  # GET /cart_products.json
-  def index
-    @cart_products = current_user.shopping_cart.cart_products.all
-  end
-
-  # GET /cart_products/1
-  # GET /cart_products/1.json
-  def show
-  end
-
-  # GET /cart_products/new
-  def new
-    # @cart_product = current_user.shopping_cart.cart_products.new
-  end
-
-  # GET /cart_products/1/edit
-  def edit
-  end
-
-  def create
-    # raise cart_product_params['product_id'].inspect
-    @cart_product = current_user.shopping_cart.cart_products.new(cart_product_params)
-    if @cart_product.save
-      # TODO: redirect to shopping cart page
-      redirect_to products_path, notice: 'Product was added to your cart successfully.' 
-    else
-      redirect_to product_path(params[:cart_product][:product_id]), alert: 'Product wasn\'t  added to your cart, please use valid inputs.'
-    end
-  end
-
-  def update
-    if get_cart_product.update(update_quantity)
-      # TODO: redirect to shopping cart page
-      redirect_to products_path, notice: 'Product was added to your cart successfully.'
-    else
-      redirect_to product_path(params[:cart_product][:product_id]), alert: 'Product wasn\'t  added to your cart, please try again later.'
-    end
-  end
-
-  # DELETE /cart_products/1
-  # DELETE /cart_products/1.json
-  def destroy
-    @cart_product.destroy
-    respond_to do |format|
-      format.html { redirect_to cart_products_url, notice: 'Cart product was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   def add_to_cart
     if !products_in_cart.present? or !get_cart_product
-      create
+      if product_stock < create_quantity
+        redirect_extra_stock
+      else
+        create
+      end
     else
       if product_stock < update_quantity[:quantity]
-        redirect_to product_path(params[:cart_product][:product_id]), alert: 'You added more than the amount available in stock.'
+        redirect_extra_stock
       else
         update
       end
@@ -84,6 +37,10 @@ class CartProductsController < ApplicationController
       cart_product_update_params
     end
 
+    def create_quantity
+      cart_product_params[:quantity].to_i
+    end
+
     def products_in_cart
       current_user.shopping_cart.cart_products
     end
@@ -95,5 +52,44 @@ class CartProductsController < ApplicationController
     def product_stock
       @product = Product.find_by_id(cart_product_params[:product_id])
       @product.stock
+    end
+
+    def redirect_extra_stock
+      redirect_to product_path(params[:cart_product][:product_id]), alert: 'You added more than the amount available in stock.'
+    end
+    
+    def redirect_error_on_action
+      redirect_to product_path(params[:cart_product][:product_id]), alert: 'Product wasn\'t  added to your cart, please try again later.'
+    end
+
+    def redirect_success_on_action
+      redirect_to shopping_cart_path, notice: 'Product was added to your cart successfully.'
+    end
+
+    def create
+      @cart_product = current_user.shopping_cart.cart_products.new(cart_product_params)
+      if @cart_product.save
+        redirect_success_on_action
+      else
+        redirect_error_on_action
+      end
+    end
+  
+    def update
+      if get_cart_product.update(update_quantity)
+        redirect_success_on_action
+      else
+        redirect_error_on_action
+      end
+    end
+  
+    # DELETE /cart_products/1
+    # DELETE /cart_products/1.json
+    def destroy
+      @cart_product.destroy
+      respond_to do |format|
+        format.html { redirect_to cart_products_url, notice: 'Cart product was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
 end
