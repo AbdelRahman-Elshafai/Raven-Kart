3a558497b85fb119a80b1786f25c3264ea26ff52
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-05-03 14:04:37 +0200

    add create and show in order controller

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,2 +1,26 @@
 class OrderController < ApplicationController
+    def create
+        # FIXME: remove from prdouct stock
+        @order = current_user.orders.new
+        if @order.save
+            @cart_products = current_user.shopping_cart.cart_products
+            # @cart_products.each(&:save!).order_products.new
+            @cart_products.each do |p|
+                @order.order_products.create(quantity: p.quantity, product_id: p.product_id, price: Product.find_by_id(p.product_id).price)
+                Product.find_by_id(p.id).decrement(:stock).save!
+            end
+            redirect_to @order, notice: 'Order was successfully created.'
+        else 
+            redirect_to shopping_cart_path, alert: 'Order not created.'
+        end
+    end
+
+    def show
+      @order = Order.find(params[:id])
+    end
+
+    private
+        def order_params
+            params.require(:order).permit()
+        end 
 end
