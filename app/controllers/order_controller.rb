0d153488a0545ec86574bafacd6e39ceb4efba66
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-05-03 18:59:23 +0200

    remove product from stock

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,10 +1,8 @@
 class OrderController < ApplicationController
     def create
-        # FIXME: remove from prdouct stock
         @order = current_user.orders.new
         if @order.save
             @cart_products = current_user.shopping_cart.cart_products
-            # @cart_products.each(&:save!).order_products.new
             @cart_products.each do |p|
                 @order.order_products.create(quantity: p.quantity, product_id: p.product_id, price: Product.find_by_id(p.product_id).price)
                 Product.find_by_id(p.product_id).decrement(:stock).save!
