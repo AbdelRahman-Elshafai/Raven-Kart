e3b0f3f1263b2a513740c8153069a4ea615f053e
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-03 19:40:05 +0200

    use get request for search route

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -8,13 +8,9 @@ class TemplateController < ApplicationController
     @search = Product.solr_search do |searcher|
       searcher.fulltext search_params[:search]
     end
-
     puts @search.results
-  end
 
-  def new_products
     render "template/shop-grid"
-
   end
 
   def search_params
