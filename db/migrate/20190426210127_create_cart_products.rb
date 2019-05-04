ff180b17c444358a1f4943a00a84ce5a91f59173
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-04 23:03:53 +0200

    add not null to cart_products' quantity, product, shopping_cart

--- db/migrate/20190426210127_create_cart_products.rb
+++ db/migrate/20190426210127_create_cart_products.rb
@@ -1,9 +1,9 @@
 class CreateCartProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_products do |t|
-      t.integer :quantity
-      t.references :product, foreign_key: true
-      t.references :shopping_cart , foreign_key:true
+      t.integer :quantity, null: false
+      t.references :product, foreign_key: true, null: false
+      t.references :shopping_cart , foreign_key: true, null: false
 
       t.timestamps
     end
