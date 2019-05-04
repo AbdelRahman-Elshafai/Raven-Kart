54c2413710f3c249ab1c921bfca943aaf8eb2d8b
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-05-04 04:18:01 +0200

    add check condition to create cart_prooduct for logged in users only

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,9 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    # TODO: guest or logged in?
-    @cart_product = current_user.shopping_cart.cart_products.new
+    if user_signed_in?
+      @cart_product = current_user.shopping_cart.cart_products.new
+    end
   end
 
   # GET /products/new
