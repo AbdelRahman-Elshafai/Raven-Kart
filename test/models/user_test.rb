57b017f23204d9ffcbbf108840c68bdec028aa32
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-05 01:46:37 +0200

    add should not create user without avatar test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should create user" do
-    user = User.new(
-      name: users(:one).name,
-      email: users(:one).email,
-      avatar: users(:one).avatar,
-      role_id: 1
-    )
-    assert user.save
+
+  test "should create user without avatar" do
+    user = User.new(name:'admin',email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password' ,address:'11-admin-admin' , role_id: 1)
+    assert_not user.save
   end
+
 end
