375e0e8477ae2c865b790bcb9c29bcae284236d7
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-04 23:06:12 +0200

    add not null to statuses' name

--- db/migrate/20190426210236_create_statuses.rb
+++ db/migrate/20190426210236_create_statuses.rb
@@ -1,7 +1,7 @@
 class CreateStatuses < ActiveRecord::Migration[5.2]
   def change
     create_table :statuses do |t|
-      t.string :type
+      t.string :type, null: false
 
       t.timestamps
     end
