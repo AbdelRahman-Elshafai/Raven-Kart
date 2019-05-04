1dc104e57a44f3db83ef764c8d1203c839eeea86
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-04 20:25:32 +0200

    add model test to brand and comment users.yml->avatar,role , products.yml->image

--- test/models/brand_test.rb
+++ test/models/brand_test.rb
@@ -4,4 +4,8 @@ class BrandTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save brand without name" do
+    brand = Brand.new
+    assert_not brand.save
+  end
 end
