19482f27001eaa5e0e95ad06e23266ace5c63e47
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-05-04 23:37:22 +0200

    create request controller

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,49 @@
+class RequestsController < ApplicationController
+        def index
+        end
+      
+        def show
+          @orders = Order.find(params[:id])
+          @products = @orders.order_products
+        end
+      
+        def confirm_status
+          confirmed_product = OrderProduct.find(params[:id])
+          confirmed_product.update(status_id: 2)
+      
+          order_products = OrderProduct.where(order_id: confirmed_product.order_id)
+          products_no = 0
+          order_products.each do |o|
+            if o.status_id == 2
+              products_no +=1
+            end
+          end
+      
+          if products_no == order_products.length
+            order = Order.find(confirmed_product.order_id)
+            order.update(status_id: 2)
+          end
+      
+          redirect_to request_path(order) , notice: 'Order confirmed.'
+        end
+      
+        def deliver_status
+          delivered_product = OrderProduct.find(params[:id])
+          delivered_product.update(status_id: 3)
+      
+          order_products = OrderProduct.where(order_id: delivered_product.order_id)
+          products_no = 0
+          order_products.each do |o|
+            if o.status_id == 3
+              products_no +=1
+            end
+          end
+      
+          if products_no == order_products.length
+            order = Order.find(delivered_product.order_id)
+            order.update(status_id: 3)
+          end
+      
+          redirect_to request_path(order), notice: 'Order delivered.'
+        end
+      end
--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,10 @@ Rails.application.routes.draw do
   get 'stores/:id' , to: 'stores#show', as: 'store'
   post 'orders', to: 'order#create', as: 'orders'
   get 'order/:id', to: 'order#show', as:'order'
-  get 'confirm_status/:id', to: 'stores#confirm_status', as: 'confirm_status'
-  get 'deliver_status/:id', to: 'stores#deliver_status', as: 'deliver_status'
+  get 'confirm_status/:id', to: 'requests#confirm_status', as: 'confirm_status'
+  get 'deliver_status/:id', to: 'requests#deliver_status', as: 'deliver_status'
+  get 'requests/:id', to: 'requests#show', as: 'request'
+
 
 
 
