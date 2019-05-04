b8dec4971c3750aef2a5b33b51ceac8091675f78
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-04 23:09:59 +0200

    add default to coupons' expired column

--- db/migrate/20190502142625_add_expired_column_to_coupons_table.rb
+++ db/migrate/20190502142625_add_expired_column_to_coupons_table.rb
@@ -1,5 +1,5 @@
 class AddExpiredColumnToCouponsTable < ActiveRecord::Migration[5.2]
   def change
-    add_column :coupons, :expired,:boolean
+    add_column :coupons, :expired,:boolean, default: 0
   end
 end
