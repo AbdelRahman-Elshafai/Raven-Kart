d5356066dbd2820f0bac8cea37627d8d46fbda09
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-04 11:31:30 +0200

    sellers facet

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -8,9 +8,11 @@ class TemplateController < ApplicationController
     @search = Sunspot.search Product do
       fulltext search_params[:search]
       with(:category_id, search_params[:category]) if search_params[:category].present?
-      with(:brand_id , search_params[:brand]) if search_params[:brand].present?
+      with(:brand_id, search_params[:brand]) if search_params[:brand].present?
+      with(:store_id, search_params[:store]) if search_params[:store].present?
       facet :category_id
       facet :brand_id
+      facet :store_id
 
     end
     @products = @search.results
@@ -23,6 +25,6 @@ class TemplateController < ApplicationController
   end
 
   def search_params
-    params.permit(:search, :category, :brand)
+    params.permit(:search, :category, :brand ,:store)
   end
 end
