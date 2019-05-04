d7d41ea09d3468657a486e9fa78f7c20142cd7e8
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-04 23:07:19 +0200

    add not null to orders' total_price , user_id , status_id

--- db/migrate/20190426210257_create_orders.rb
+++ db/migrate/20190426210257_create_orders.rb
@@ -2,10 +2,10 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
       t.datetime :purchased_at
-      t.integer :total_price
+      t.integer :total_price, null: false
       t.integer :total_price_after_sale
-      t.references :user, foreign_key: true
-      t.references :status, foreign_key: true
+      t.references :user, foreign_key: true, null: false
+      t.references :status, foreign_key: true, null: false
       t.references :coupon, foreign_key: true
 
       t.timestamps
