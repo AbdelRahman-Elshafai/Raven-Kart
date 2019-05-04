9c93b5c03bea5fd9fc335ddd221ed3a242739524
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-05-04 21:54:49 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_100519) do
+ActiveRecord::Schema.define(version: 2019_05_03_192030) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -155,7 +155,7 @@ ActiveRecord::Schema.define(version: 2019_05_03_100519) do
   end
 
   create_table "statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
