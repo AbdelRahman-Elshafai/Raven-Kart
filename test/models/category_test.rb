2cb642e561e2ecf19b0ed459c1b66c5539c4b5f2
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-04 20:36:46 +0200

    add model test to category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save brand without name" do
+    category = Category.new
+    assert_not category.save
+  end
+
 end
