84174ab5015578f06ceaf6bf4f5a89c441e317a4
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-04 23:05:42 +0200

    add not null to feedbacks' rating , user_id , product_id

--- db/migrate/20190426210220_create_feedbacks.rb
+++ db/migrate/20190426210220_create_feedbacks.rb
@@ -2,9 +2,9 @@ class CreateFeedbacks < ActiveRecord::Migration[5.2]
   def change
     create_table :feedbacks do |t|
       t.text :review
-      t.integer :rating
-      t.references :user, foreign_key: true
-      t.references :product, foreign_key: true
+      t.integer :rating, null: false
+      t.references :user, foreign_key: true, null: false
+      t.references :product, foreign_key: true, null: false
 
       t.timestamps
     end
