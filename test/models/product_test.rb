78557d95efe81f0c8b1a9a65894c97906df00601
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-05 05:15:58 +0200

    add product test should not create product without brand

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -51,6 +51,15 @@ class ProductTest < ActiveSupport::TestCase
     assert_not @product.save
   end
 
+  test "should not create product without brand" do
+    @product = Product.new( title:'title',description: 'description',price:10,stock:10)
+    @product.category = categories(:electronics)
+    @product.store = stores(:bla)
+    @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
+    @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
+    assert_not @product.save
+  end
+
 
 
 end
