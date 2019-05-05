dfa344402dd7a73d13ea24f4fbb11341b6a1ab1c
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-05 04:57:03 +0200

    add should not create product without title

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -10,4 +10,15 @@ class ProductTest < ActiveSupport::TestCase
     @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
     assert @product.save
   end
+
+  test "should not create product without title" do
+    @product = Product.new( description: 'description',stock: 10,price: 100)
+    @product.brand = brands(:dell)
+    @product.category = categories(:electronics)
+    @product.store = stores(:bla)
+    @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
+    @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
+    assert_not @product.save
+  end
+
 end
