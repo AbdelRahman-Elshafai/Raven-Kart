f22dbbb270fdaa2c448bd6ccfe2d3ef854284f2d
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-05 05:18:48 +0200

    add product test should not create product without images

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,12 +4,6 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should create product" do
-    @product = products(:product)
-    @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
-    @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
-    assert @product.save
-  end
 
   test "should not create product without title" do
     @product = Product.new( description: 'description',stock: 10,price: 100)
@@ -77,6 +71,22 @@ class ProductTest < ActiveSupport::TestCase
     @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
     assert_not @product.save
   end
+
+  test "should not create product without images" do
+    @product = Product.new( title:'title',description: 'description',stock:10)
+    @product.brand = brands(:dell)
+    @product.category = categories(:electronics)
+    @product.store = stores(:bla)
+    assert_not @product.save
+  end
+
+  test "should create product" do
+    @product = products(:product)
+    @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
+    @product.images.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/png")
+    assert @product.save
+  end
+
 
 
 
