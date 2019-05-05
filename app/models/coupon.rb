00d779de67af1ce7ee45e768b8bf5f7ecf6f0b87
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-05-05 22:00:32 +0200

    coupon validation

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,11 @@
 class Coupon < ApplicationRecord
     has_many :orders
+
+    validates :name , presence: true
+    validates :expiration_date , presence: true
+    validates :usage_limit , presence: true
+    validates :deduction_type , presence: true
+    validates :deduction_amount , presence: true
+
+
 end
