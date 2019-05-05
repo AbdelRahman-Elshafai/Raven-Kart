4476c62a40349e67ff801cb6897cb5852697e549
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-05-05 03:48:12 +0200

    add should create brand test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,14 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should create product" do
+    product = Product.create(
+                         title:'title', description:'description', stock:10,
+                         price:10, brand_id:1
+    )
+    product.images.attach(io: File.open('public/apple-touch-icon.png'),
+                          filename: 'apple-touch-icon.png',
+                          content_type: 'application/png')
+    assert product.save
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,9 +2,18 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "should create user without avatar" do
+  test "should not create user without name" do
+    user = User.new(email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password' ,address:'11-admin-admin' , role_id: 1)
+    assert_not user.save
+  end
+
+  test "should not create user without avatar" do
     user = User.new(name:'admin',email: 'admin@example.com', password: 'password',
                     password_confirmation: 'password' ,address:'11-admin-admin' , role_id: 1)
+
+    # user.avatar.attach(io: File.open("public/apple-touch-icon.png"), filename: "apple-touch-icon.png", content_type: "image/jpg")
+
     assert_not user.save
   end
 
