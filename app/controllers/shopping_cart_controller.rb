fbda8aaa414ac129d4e68df3d4360db6a7938bf2
mahydit <mahydit_anber@outlook.com>
2019-05-06 03:12:15 +0200

    use  load_and_authorize_resourc

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -1,9 +1,7 @@
 class ShoppingCartController < ApplicationController
-  authorize_resource
+  load_and_authorize_resource
   before_action :map_products_details_in_cart, :products_in_cart, :calculate_products_total, only: [:show]
   before_action :authenticate_user!
-
-
 
   def show
   end
