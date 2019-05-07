0239a6528bebe421fb0d4610ec58f70f2e857f78
mahydit <mahydit_anber@outlook.com>
2019-05-07 07:29:38 +0200

    bug fix: add cproduct to cart from index page

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -1,5 +1,14 @@
 class TemplateController < ApplicationController
+  before_action :create_cart_product_for_user
+
   def index
     @product = Product.first()
+  end
+
+  private
+  def create_cart_product_for_user
+    if user_signed_in? and !current_user.seller?
+      @cart_product = current_user.shopping_cart.cart_products.new
+    end
   end
 end
